a4e830291deb6e747bcbff2142a82c6a3c3537bb
Max Genson <maxgenson@pop-os.localdomain>
2022-01-18 18:49:47 +0100

    first commit

---
+++ src/fractal.glsl
@@ -0,0 +1,266 @@
+#version 150 core
+
+// depiction variables
+vec2 z = vec2(0.0, 0.0);
+vec2 c = vec2(0.0, 0.0);
+uniform float zoom;
+uniform float c_x;
+uniform float c_y;
+vec2 c_complex = vec2(c_x, c_y);
+uniform int max_iterations;
+
+// color palette and algorithms variables
+uniform int color_algorithm;
+float color_offset = 0.0;
+float color_range = 0.08;
+
+// window variables
+uniform int width;
+uniform int height;
+vec2 w_dimension = vec2(width, height);
+
+// julia or mandelbrot
+uniform int set;
+uniform int fractal_algorithm;
+
+// main color output
+out vec4 color;
+
+//-----------------------------------
+// Complex numbers utility
+//-----------------------------------
+
+/**
+ *
+ * Complex math functions
+ *
+        ((a.x*a.x)+(a.y*a.y))
+
+    from Johan Karlsson
+    found here: https://github.com/julesb/glsl-util/blob/master/complexvisual.glsl
+ */
+
+#define PI 3.14159265
+#define TWOPI (2.0*PI)
+
+#define cx_add(a, b) vec2(a.x + b.x, a.y + b.y)
+#define cx_sub(a, b) vec2(a.x - b.x, a.y - b.y)
+#define cx_mul(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
+#define cx_div(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
+#define cx_modulus(a) length(a)
+#define cx_conj(a) vec2(a.x,-a.y)
+#define cx_arg(a) atan(a.y,a.x)
+#define cx_sin(a) vec2(sin(a.x) * cosh(a.y), cos(a.x) * sinh(a.y))
+#define cx_cos(a) vec2(cos(a.x) * cosh(a.y), -sin(a.x) * sinh(a.y))
+
+vec2 cx_to_polar(vec2 a) {
+    float phi = atan(a.x, a.y);
+    float r = sqrt(a.x * a.x + a.y * a.y);
+    return vec2(r, phi);
+}
+
+vec2 cx_sqrt(vec2 a) {
+    float r = sqrt(a.x * a.x + a.y * a.y);
+    float rpart = sqrt(0.5 * (r + a.x));
+    float ipart = sqrt(0.5 * (r - a.x));
+    if(a.y < 0.0)
+        ipart = -ipart;
+    return vec2(rpart, ipart);
+}
+
+vec2 cx_tan(vec2 a) {
+    return cx_div(cx_sin(a), cx_cos(a));
+}
+
+vec2 cx_log(vec2 a) {
+    float rpart = sqrt((a.x * a.x) + (a.y * a.y));
+    float ipart = atan(a.y, a.x);
+    if(ipart > PI)
+        ipart = ipart - (2.0 * PI);
+    return vec2(log(rpart), ipart);
+}
+
+vec2 cx_mobius(vec2 a) {
+    vec2 c1 = a - vec2(1.0, 0.0);
+    vec2 c2 = a + vec2(1.0, 0.0);
+    return cx_div(c1, c2);
+}
+
+vec2 cx_z_plus_one_over_z(vec2 a) {
+    return a + cx_div(vec2(1.0, 0.0), a);
+}
+
+vec2 cx_z_squared_plus_c(vec2 z, vec2 c) {
+    return cx_mul(z, z) + c;
+}
+
+vec2 cx_sin_of_one_over_z(vec2 z) {
+    return cx_sin(cx_div(vec2(1.0, 0.0), z));
+}
+
+//-----------------------------------
+// Misc ultility
+//-----------------------------------
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+float log10(float num) {
+    return log(num) / log(10.0);
+}
+
+//-----------------------------------
+// Coloring algorithms
+//-----------------------------------
+
+void linear_coloring(float iterations) {
+    if(iterations == max_iterations) {
+        color = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        float val = iterations / float(max_iterations);
+        color = vec4(hsv2rgb(vec3(val, 1.0, 1.0)), 1.0);
+    }
+}
+
+void smooth_coloring(float iterations, vec2 z) {
+    if(iterations == max_iterations) {
+        color = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        float val = iterations + 1.0 - log10(log10(length(z))) / log10(2.0);
+        val = val * (1.0 / float(max_iterations));
+        color = vec4(hsv2rgb(vec3(val, 1.0, 1.0)), 1.0);
+    }
+}
+
+void shady_patterns(float iterations, vec2 z) {
+    if(iterations == max_iterations) {
+        color = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        float min_step = iterations / float(max_iterations);
+        float val = min_step + 1.0 - log(log(length(z))) / log(2.0);
+        val = color_offset + val * color_range;
+        color = vec4(hsv2rgb(vec3(val, 1.0, 1.0)), 1.0);
+    }
+}
+
+//-----------------------------------
+// Fractal algorithms
+//-----------------------------------
+
+void naive_mandelbrot(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        z = cx_mul(z, z) + c;
+        if(length(z) > 2.0) {
+            break;
+        }
+    }
+}
+
+void optimized_mandelbrot(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        vec2 z2 = vec2(0.0, 0.0);
+        z2 = z.xy * z.xy;
+        z.y = sin((z.x + z.x) * z.y) + tan(c.y);
+        z.x = (z2.x - z2.y) + log(sin((1.618*(c.x * z.y)/128.0)));
+        if(z2.x + z2.y > 50.0) {
+            break;
+        }
+    }
+}
+
+// cool points
+// re: 0.0062482357, im: 0.559965
+void mobius_squared(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        z = cx_mobius(cx_mul(z, z) + c);
+        if(length(z) > 4.0) {
+            break;
+        }
+    }
+}
+
+// cool points
+// re: 0.11892238, im: 0.4829234
+void mobius_cubed(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        z = cx_mobius(cx_mul(cx_mul(z, z), z) + c);
+        if(length(z) > 2.0) {
+            break;
+        }
+    }
+}
+
+void mobius_z_squared(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        z = cx_mobius(cx_mul(z, z)) + c;
+        if(length(z) > 2.0) {
+            break;
+        }
+    }
+}
+
+void mobius_z_cubed(inout float iterations, inout vec2 z, inout vec2 c) {
+    for(iterations = 0.0; iterations < max_iterations; iterations++) {
+        z = cx_mobius(cx_mul(cx_mul(z, z), z)) + c;
+        if(length(z) > 2.0) {
+            break;
+        }
+    }
+}
+
+//-----------------------------------
+// Main Loop
+//-----------------------------------
+
+void main() {
+
+    float iterations;
+
+    switch(set) {
+        case 0:
+        // Mandelbrot Set
+            c = c_complex.xy + ((gl_FragCoord.xy / float(w_dimension.xy)) - 0.5) * zoom;
+            z = c;
+            break;
+        case 1:
+        // Julia Set
+            z = ((gl_FragCoord.xy / float(w_dimension.xy)) - 0.5) * zoom;
+            c = c_complex;
+            break;
+    }
+
+    switch(fractal_algorithm) {
+        case 0:
+            optimized_mandelbrot(iterations, z, c);
+            break;
+        case 1:
+            mobius_squared(iterations, z, c);
+            break;
+        case 2:
+            mobius_cubed(iterations, z, c);
+            break;
+        case 3:
+            mobius_z_squared(iterations, z, c);
+            break;
+        case 4:
+            mobius_z_cubed(iterations, z, c);
+            break;
+    }
+
+    //mobius_squared(iterations, z, c);
+
+    switch(color_algorithm) {
+        case 0:
+            linear_coloring(iterations);
+            break;
+        case 1:
+            smooth_coloring(iterations, z);
+            break;
+        case 2:
+            shady_patterns(iterations, z);
+            break;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,245 @@
+#[macro_use]
+extern crate glium;
+
+use glium::backend::glutin::glutin::dpi::PhysicalPosition;
+use glium::glutin::event::WindowEvent::MouseInput;
+use glium::glutin::event::{ElementState, MouseButton, VirtualKeyCode};
+use glium::glutin::*;
+use glium::{glutin, Surface};
+
+fn main() {
+    let mut width: i32 = 400;
+    let mut height: i32 = 400;
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let size: glutin::dpi::LogicalSize<u32> = (width, height).into();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_title("Mandelbrot on GPU")
+        .with_inner_size(size);
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let vertex_shader = include_str!("vertex.glsl");
+    let fragment_shader = include_str!("fractal.glsl");
+
+    // Compile shaders
+    let program =
+        glium::Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    // Render 2 triangles covering the whole screen
+    let vertices = [
+        // Top-left corner
+        Vertex {
+            position: [-1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, 1.0],
+        },
+        Vertex {
+            position: [-1.0, -1.0],
+        },
+        // Bottom-right corner
+        Vertex {
+            position: [-1.0, -1.0],
+        },
+        Vertex {
+            position: [1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, -1.0],
+        },
+    ];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &vertices).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // depiction variables
+    let mut zoom: f32 = 4.0;
+    let mut c_x: f32 = 0.0;
+    let mut c_y: f32 = 0.0;
+    let mut max_iterations: i32 = 70;
+
+    // keyboard input variables
+    let mut dragging = false;
+    let mut mouse_x: f32 = 0.0;
+    let mut mouse_y: f32 = 0.0;
+    let mut drag_offset_x: f32 = 0.0;
+    let mut drag_offset_y: f32 = 0.0;
+
+    // modes
+    let mut color_algorithm: i32 = 0;
+    let mut set: i32 = 0;
+    let mut fractal_algorithm: i32 = 0;
+
+    // Run Window
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            // User input gets handled in this arm
+            event::Event::WindowEvent { event, .. } => match event {
+                // Close the window
+                event::WindowEvent::CloseRequested => {
+                    *control_flow = event_loop::ControlFlow::Exit;
+                    return;
+                }
+
+                // When hitting Space
+                // Switch between coloring alorithms
+                event::WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Space),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                } => match color_algorithm {
+                    0 => color_algorithm = 1,
+                    1 => color_algorithm = 2,
+                    2 => color_algorithm = 0,
+                    _ => return,
+                },
+
+                // When hitting Tab
+                // Switch between fractal algorithms
+                event::WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Tab),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                } => match fractal_algorithm {
+                    0 => fractal_algorithm = 1,
+                    1 => fractal_algorithm = 2,
+                    2 => fractal_algorithm = 3,
+                    3 => fractal_algorithm = 4,
+                    4 => fractal_algorithm = 0,
+                    _ => return,
+                },
+
+                // When hitting Enter/Return
+                // Switch between julia or mandelbrot set
+                event::WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Return),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                } => match set {
+                    0 => {
+                        set = 1;
+                        println!("re: {}, im: {}", drag_offset_x, drag_offset_y);
+                    }
+                    1 => {
+                        set = 0;
+                        println!("re: {}, im: {}", drag_offset_x, drag_offset_y);
+                    }
+                    _ => return,
+                },
+
+                // Do something when the mouse is moving
+                glutin::event::WindowEvent::CursorMoved {
+                    position: PhysicalPosition { x, y },
+                    ..
+                } => {
+                    let x = x as f32;
+                    let y = y as f32;
+                    mouse_x = x;
+                    mouse_y = y;
+                }
+
+                // Do something when the left mouse button is pressed or released
+                MouseInput {
+                    state: ElementState::Pressed,
+                    button: MouseButton::Left,
+                    ..
+                } => {
+                    dragging = true;
+                    drag_offset_x = (mouse_x / width as f32 - 0.5) * zoom + c_x;
+                    drag_offset_y = (mouse_y / height as f32 - 0.5) * zoom - c_y;
+                }
+                MouseInput {
+                    state: ElementState::Released,
+                    button: MouseButton::Left,
+                    ..
+                } => dragging = false,
+
+                // Do something when the mouse wheel is being used
+                glutin::event::WindowEvent::MouseWheel {
+                    delta: glutin::event::MouseScrollDelta::LineDelta(_source, direction),
+                    ..
+                } => {
+                    if direction == 1.0 {
+                        zoom *= 0.9;
+                    }
+                    if direction == -1.0 {
+                        zoom *= 1.0 / 0.9;
+                    }
+                }
+
+                // else
+                _ => return,
+            },
+            event::Event::NewEvents(cause) => match cause {
+                event::StartCause::ResumeTimeReached { .. } => (),
+                event::StartCause::Init => (),
+                _ => return,
+            },
+
+            _ => (),
+        }
+
+        if dragging {
+            c_x = -(mouse_x / width as f32 - 0.5) * zoom + drag_offset_x;
+            c_y = (mouse_y / height as f32 - 0.5) * zoom - drag_offset_y;
+        }
+
+        // Frame Time
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        // update variables in this frame
+        zoom = zoom;
+        max_iterations = max_iterations;
+        c_x = c_x;
+        c_y = c_y;
+        width = width;
+        height = height;
+
+        // update frame buffer
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        let uniforms = uniform! {
+            zoom: zoom,
+            c_x: c_x,
+            c_y: c_y,
+            max_iterations: max_iterations,
+            color_algorithm: color_algorithm,
+            width: width,
+            heigth: height,
+            set: set,
+            fractal_algorithm: fractal_algorithm
+        };
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
+}
